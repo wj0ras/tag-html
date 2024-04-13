f719ef1b2b11a92abc1993ff155eb22af86513fd
Welison Joras <welison.joras@hotmail.com>
2024-04-12 21:59:04 -0300

    novo componente criado TituloDinamico

---
+++ src/components/TituloDinamico.js
@@ -0,0 +1,21 @@
+class TituloDinamico extends HTMLElement {
+    constructor() {
+        super();
+        const shadow = this.attachShadow({mode: "open"});
+
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = "Welison";
+
+        const style = document.createElement("style");
+        style.textContent = `
+        h1 {
+            color: red;
+        }
+        `;
+
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
+    }
+}
+
+customElements.define("titulo-dinamico", TituloDinamico);
